e174194214a156498a5cd4acb97faa917f1f99d3
Anton_Karpushko <habrabro@gmail.com>
2021-07-31 14:43:20 +0300

    develop: added ImageCaching protocol.

---
+++ JMImageLoader/ImageCaching.swift
@@ -0,0 +1,15 @@
+//
+//  ImageCaching.swift
+//  JMImageLoader
+//
+//  Created by macbook on 31.07.2021.
+//
+
+import UIKit
+
+protocol ImageCaching {
+    subscript(_ url: URL) -> UIImage? { get set }
+    
+    func removeImage(by url: URL)
+    func removeAllImages()
+}
