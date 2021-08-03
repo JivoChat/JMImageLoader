29ae8f085b8fe431ed6bb3fafa5807062cf4682d
Anton_Karpushko <habrabro@gmail.com>
2021-08-03 20:01:04 +0300

    develop: - initialized a Pods project to test local JMImageLoader integration to JMImageLoaderTestApp.

---
+++ Pods/Target Support Files/Pods-JMImageLoader/Pods-JMImageLoader-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_JMImageLoader : NSObject
+@end
+@implementation PodsDummy_Pods_JMImageLoader
+@end
---
+++ Pods/Target Support Files/Pods-JMImageLoaderTestAppTests/Pods-JMImageLoaderTestAppTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_JMImageLoaderTestAppTests : NSObject
+@end
+@implementation PodsDummy_Pods_JMImageLoaderTestAppTests
+@end
