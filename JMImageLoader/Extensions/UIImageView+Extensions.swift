1f5c31ea30fb143c92f6784df3ff29b000463cd8
Stan Potemkin <bronenos@gmail.com>
2021-10-06 12:10:29 +0300

    Add the Image loading spinner

--- JMImageLoader/Extensions/UIImageView+Extensions.swift
+++ JMImageLoader/Extensions/UIImageView+Extensions.swift
@@ -10,26 +10,15 @@ import UIKit
 extension UIImageView: JMImageViewImageLoading {
     @discardableResult
     public func jmLoadImage(with url: URL, completion: ((Result<UIImage, Error>) -> Void)? = nil) -> JMImageLoadingCancellable {
-        let IMAGE_CACHE_MEMORY_LIMIT = 1024 * 1024 * 50
-        
-        let defaultLoadingStrategy = ImageLoadingStrategyFactory.defaultShared(withImageCacheMemoryLimit: IMAGE_CACHE_MEMORY_LIMIT)
-        let task = defaultLoadingStrategy.load(with: url) { [weak self] result in
-            switch result {
-            case let .success(image):
-                self?.image = image
-                
-            case .failure:
-                break
-            }
-            
-            completion?(result)
-        }
-        
-        return task
+        let defaultLoadingStrategy = ImageLoadingStrategyFactory.defaultShared(withImageCacheMemoryLimit: 1024 * 1024 * 50)
+        return jmLoadImage(with: url, usingStrategy: defaultLoadingStrategy, completion: completion)
     }
     
     @discardableResult
     public func jmLoadImage(with url: URL, usingStrategy loadingStrategy: JMImageLoading, completion: ((Result<UIImage, Error>) -> Void)? = nil) -> JMImageLoadingCancellable {
+        let indicator = retrieveIndicator()
+        indicator.isHidden = false
+        
         let task = loadingStrategy.load(with: url) { [weak self] result in
             switch result {
             case let .success(image):
@@ -39,9 +28,36 @@ extension UIImageView: JMImageViewImageLoading {
                 break
             }
             
+            indicator.isHidden = true
             completion?(result)
         }
         
         return task
+    }
+    
+    private func retrieveIndicator() -> UIActivityIndicatorView {
+        let activityTag = 0xF180
+        
+        if let control = viewWithTag(activityTag) as? UIActivityIndicatorView {
+            return control
+        }
+        else {
+            let control = UIActivityIndicatorView()
+            insertSubview(control, at: 0)
+            
+            if #available(iOS 13.0, *) {
+                control.style = .medium
+            }
+            else {
+                control.style = .white
+            }
+            
+            control.frame = bounds
+            control.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+            control.tag = activityTag
+            control.startAnimating()
+
+            return control
+        }
     }
 }
